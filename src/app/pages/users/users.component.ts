641e3ac45e8dd021d2abcbb1f0a0e3a0296d41be
yogi gratz <yogi@localhost.localdomain>
2026-03-31 04:16:05 +0000

    Added user list

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
 
+  constructor(private http: HttpClient) {}
+  
+  users: any[] = [];
+  
+    ngOnInit(): void {
+  
+    const token = localStorage.getItem('tt_token');
+  
+    if (!token) {
+      console.error('No token found');
+      return;
+    }
+  
+    this.http.get<any[]>(environment.apiBaseUrl + '/api/Registration',
+     {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+     })
+        .subscribe(data => {
+          this.users = data;
+          console.log("Candidates:", data);
+        });
+  
+    }
+  
 }
